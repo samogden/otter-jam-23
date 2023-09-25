e7216bef1341376ca4d0c6b7c91b6f29cc447514
Sam O (Prof) <samuel.s.ogden@gmail.com>
2023-09-25 10:49:21 -0700

    Cleaning up code and adding an ignore

--- game-jam.py
+++ game-jam.py
@@ -15,7 +15,7 @@ import helper
 from helper import CostTracker
 
 logging.basicConfig()
-logging.getLogger().setLevel(logging.DEBUG)
+logging.getLogger().setLevel(logging.INFO)
 
 class OpenAIInterface(object):
   cost_tracker = CostTracker()
@@ -154,36 +154,14 @@ def run_adventure(adventure_hook):
   
 
 def main():
+  print("Welcome!")
   OpenAIInterface.get_api_key()
-  
-  adventure_hook = "I am entering a fantasy tavern to talk to the bartender about getting a cup of ale and possibly some quests.  The bartender, played by the asisstant, has a gruff personality but is willing to take my money."
+  print("Starting adventure....")
+  adventure_hook = "I am entering a fantasy tavern to talk to the bartender about getting a cup of ale and possibly some quests.  The bartender, played by the assistant, has a gruff personality but is willing to take my money."
   
   run_adventure(adventure_hook)
   
   return
-  
-  #conv = Conversation("I am entering a fantasy tavern to talk to the bartender about getting a cup of ale and possibly some quests.  The bartender, played by the assistant, has a gruff personality but is willing to take my money.")
-  conv = Conversation("The previous interaction is summarized as follows.  You, the assistant, are playing the bartender Gruff.  In the fantasy tavern, the weary traveler approaches the gruff bartender, requesting a flagon of grog and inquiring about any local news. The bartender mentions rumors of bandits causing trouble in the nearby forests and a reward being offered to those who can put an end to their mischief. Another quest involves retrieving a rare magical artifact from a cursed tomb, with a wizard named Zephyrus providing more information. Intrigued, the traveler asks about the rewards for completing the quest. The bartender explains that Zephyrus promises a substantial sum of gold, the potential to enhance one's abilities with the artifact, and the opportunity to gain a reputation as a skilled adventurer. Satisfied with the potential rewards, the traveler expresses interest in pursuing the quest and asks for the bartender's name for future reference. The bartender introduces himself as Gruff, assuring the traveler that he'll likely find him at the tavern upon their return. With gratitude and determination, the traveler sets off to locate the wizard and embark on the perilous quest.")
-  while True:
-    msg = input("> ")
-    if msg.strip().lower() == "exit":
-      conv.summarize()
-      break
-    elif msg.strip().lower() == "new":
-      conv = Conversation
-      continue
-    elif msg.startswith("image:"):
-      prompt = ':'.join(msg.split(':')[1:])
-      conv.get_image(prompt)
-    elif msg.startswith("image variation"):
-      prompt = ':'.join(msg.split(':')[1:])
-      conv.image_variation(prompt)
-    else:
-      resp = conv.send_msg(msg)
-      print(resp)
-    logging.info(f"Total cost: ${conv.cost_tracker.cumulative_cost : 0.6f}")
-    print("")
-  print(f"Summary:\n{conv.summary}")
 
 if __name__ == "__main__":
   main()
